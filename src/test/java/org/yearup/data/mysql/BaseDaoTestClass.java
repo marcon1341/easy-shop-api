202238bae5351ece2dbbbe0938199e3d1b7b19cc
marcon <marconsolomonyadete@gmail.com>
2025-06-24 16:48:04 -0700

    added Test Property

--- src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
+++ src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.yearup.configuration.TestDatabaseConfig;
 
@@ -13,6 +14,7 @@ import java.sql.SQLException;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestDatabaseConfig.class)
+@TestPropertySource(locations = "classpath:application.properties")
 public abstract class BaseDaoTestClass
 {
     @Autowired
@@ -21,10 +23,10 @@ public abstract class BaseDaoTestClass
     @AfterEach
     public void rollback() throws SQLException
     {
-        Connection connection = dataSource.getConnection();
-        if(!connection.getAutoCommit())
-        {
-            dataSource.getConnection().rollback();
+        try (Connection connection = dataSource.getConnection()) {
+            if (!connection.getAutoCommit()) {
+                connection.rollback();
+            }
         }
     }
 }
